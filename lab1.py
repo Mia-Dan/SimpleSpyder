c7dd630e70266fd97300b7b1e96b34484d95dc30
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2021-08-07 22:04:21 +0800

    update progress

--- lab1.py
+++ lab1.py
@@ -1,5 +1,7 @@
 from dl1 import filename2dir, get_html_from_local, save_to_local
 from bs4 import BeautifulSoup as bs
+import os
+
 '''
 本段代码为prototype。
 想解决的问题：
@@ -9,36 +11,97 @@ from bs4 import BeautifulSoup as bs
 	访问这些页面对应的网址，速度也很慢（如果不是超时无法打开）。
 推测：
 	页面某些地方渲染需要网络。
+==========
+0807更新：
+	更加精确的内容定位。
+==========
+问题再分析：
+	部分页面，如29.html，涉及图片；
+	然而本地保存的html内未包含图片，需联网载入；
+	“断开网络后本地页面加载很快”是因为断网后不再尝试加载图片。
+下一步：
+要解决的问题：
+	下载器应能保存图片。
+推测：
+	修改get_html_from_url()，使能保存图片。
+	推测会进一步涉及request库的使用。
 '''
 
-# # a prototype that works
-# # html = get_html_from_local("Computational Fairy Tales Stories by Level.html") 
-# html = get_html_from_local("10.txt")
+# # 0805 ver: a prototype that works
+# # 思路：删减不必要的板块，提高页面打开速度。
+# for i in range(87): # 0.txt to 86.txt
+# 	filename = str(i)+".txt"
+# 	html = get_html_from_local(filename)
+# 	soup = bs(html, features="lxml")
+# 	target_index = 5
+# 	temp_tag = soup.body.contents[target_index]
+# 	save_to_local(temp_tag.prettify(), filename)
+
+# 	print(i) # show current progress
+
+
+# 0807 ver: a prototype that works
+# 思路：锁定需要的tag，保存它们。
+
+def save_taglist_to_local(taglist, filename):
+	str1 = ''
+	for tag1 in taglist:
+		print(type(tag1.prettify()))
+		str1 = str1 + '\n' + tag1.prettify()
+	save_to_local(str1, filename)
+
+# # prototype 0807-1
+# # 测试用例
+# html = get_html_from_local("10.html")
 # soup = bs(html, features="lxml")
-# a = soup.body.contents
-#
-# # print(len(a)) # 13
-# i = 5
-# # a[1],a[7] is useless at my purpose; 
-# # even ones like a[0], a[2], ... are empty; 
-# # only a[5] contains target content.
-# print(a[i])
-# print(a[i].name)
-# print(type(a[i])) # <class 'bs4.element.Tag'>
-# print(type(a[i].prettify())) # <class 'str'>
-# # and a[i].text would throw away all the format, which is bad here.
+# # manually: 通过观察html文件，分析出目标结点的特征（e.g. tag的class）
+# # 取出这些结点，保存为列表taglist
+# taglist = []
+# taglist.extend(soup.find_all(class_ = 'date-header')) 
+# taglist.extend(soup.find_all(class_ = 'post-title entry-title')) 
+# taglist.extend(soup.find_all(class_ = 'post-body entry-content')) 
+# taglist.extend(soup.find_all(class_ = 'comments')) 
+# taglist.extend(soup.find_all(class_ = 'post-labels')) 
 
-# save_to_local(a[i].prettify(), "a"+str(i)+"_1.html") # reason: see above.
+# # 展示列表taglist，检查是否正确
+# i = 0; ylist = []
+# for tag1 in taglist:
+# 	# show
+# 	os.system("clear")
+# 	print(tag1)
+# 	print(type(tag1))
+# 	print('No. '+str(i))
+# 	i = i+1
 
+# 	# check
+# 	answer = input('''
+# Yes and continue? - type enter/n
+# No and continue? - type 'n'+enter/n
+# Quit? - type ‘q’+enter
+# ''')
+# 	if answer == "n":
+# 		continue
+# 	if answer == "q":
+# 		break
+# 	ylist.append(tag1)
+
+# save_taglist_to_local(ylist,'10_test.html')
+
+# prototype 0807-2
 for i in range(87): # 0.txt to 86.txt
-	filename = str(i)+".txt"
+	filename = str(i)+".html"
 	html = get_html_from_local(filename)
 	soup = bs(html, features="lxml")
-	target_index = 5
-	temp_tag = soup.body.contents[target_index]
-	save_to_local(temp_tag.prettify(), filename)
+	# selected content
+	taglist = []
+	taglist.extend(soup.find_all(class_ = 'date-header')) 
+	taglist.extend(soup.find_all(class_ = 'post-title entry-title')) 
+	taglist.extend(soup.find_all(class_ = 'post-body entry-content')) 
+	taglist.extend(soup.find_all(class_ = 'comments')) 
+	taglist.extend(soup.find_all(class_ = 'post-labels')) 
+	save_taglist_to_local(taglist, filename)
 
-	print(i)
+	print(i) # show current progress
 
 
 print("done")
